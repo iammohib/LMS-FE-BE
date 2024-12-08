31a9000cbd997b013d8d4f29905001ea9512943a
mohib <itsmohibuddin@gmail.com>
2024-12-08 18:39:13 +0530

    fixes: mongo

--- configs/dbConn.js
+++ configs/dbConn.js
@@ -11,9 +11,7 @@ mongoose.set("strictQuery", false);
 
 const connectToDB = async () => {
   try {
-    const { connection } = await mongoose.connect(
-      process.env.MONGO_URI || `mongodb://127.0.0.1:27017/lms`
-    );
+    const { connection } = await mongoose.connect(process.env.MONGO_URI);
 
     if (connection) {
       console.log(`Connected to MongoDB: ${connection.host}`);
